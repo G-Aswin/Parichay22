77632ba851879f5b810c561bf092f2ced2b32367
G. Aswin <aswin.g.rns@gmail.com>
2022-06-15 14:07:47 +0530

    coming soon

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('comingsoon.html')
+#    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
