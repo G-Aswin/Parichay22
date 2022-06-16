09d1cafe395413e6516553b1a9ef18bab8b99e99
G. Aswin <aswin.g.rns@gmail.com>
2022-06-16 09:25:58 +0530

    Prefest Events Registration

--- app.py
+++ app.py
@@ -6,24 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('comingsoon.html')
-#    return render_template('index.html')
+   # return render_template('index.html')
+   return redirect("/prefest")
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/prefest')
+def prefest():
+   return render_template('prefest.html')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+# @app.route('/events')
+# def events():
+#    print('Request for events page received')
+#    return render_template('events.html')
 
 
 if __name__ == '__main__':
