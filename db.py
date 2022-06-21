7d8c8c54b6bbea04fa4399c90cd043e86ab0dbd1
Aditya Shrivastava <aditya0309raj@gmail.com>
2022-06-21 12:08:50 +0530

    event db updated

--- db.py
+++ db.py
@@ -1,10 +1,42 @@
+import collections
 from flask_pymongo import pymongo
 
 client = pymongo.MongoClient("mongodb+srv://cat-rnsit:parichay2022@cluster0.cwu4w3u.mongodb.net/?retryWrites=true&w=majority")
 db = client.get_database('parichay')
 
+events = db.events
+department = db.department
+winners = db.winners
+
+# """command for printing all the document within event"""
+
+# event_list_all = events.find({})
+# for event in event_list_all:
+# 	print(event,end="\n\n")
+
+
+# printing list of events with their respective date
+
+event_list = events.find({},{'_id':0,'event_name':1,'date':1})
+e_l = list()
+for event in event_list:
+	# print(f"{event['event_name']} is on {event['date']}")
+	e_l.append(f"{event['event_name']} is on {event['date']}")
+
+for e in e_l:
+	print(e)
+
+
+
+
+
+
+
+
 """ 
 Parichay
+
+format of database
 
 events:
 	event_id
@@ -28,11 +60,3 @@ winners:
 	points_scored
    
 """
-
-events = db.events
-department = db.department
-winners = db.winners
-
-"""next task is to add the json document in these databases  in the same order given above""" 
-
-print(events.count_documents({}))
